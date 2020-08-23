b1946af7b136922b8c9e7be41820da17057bb191
umangjajodia <umangjajodia4@gmail.com>
2020-08-23 20:08:13 +0530

    email service down,commenting all emailcode

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,27 +1,27 @@
-const sgMail = require('@sendgrid/mail');
+// const sgMail = require('@sendgrid/mail');
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 
-const sendWelcomeEmail = (email,name)=>{
-    sgMail.send({
-        to: email,
-        from: 'umangjajodia4@gmail.com',
-        subject: 'thanks for joining',
-        text: 'Welcome to the app, $(name). Let me know how you get along with the app.'
+// const sendWelcomeEmail = (email,name)=>{
+//     sgMail.send({
+//         to: email,
+//         from: 'umangjajodia4@gmail.com',
+//         subject: 'thanks for joining',
+//         text: 'Welcome to the app, $(name). Let me know how you get along with the app.'
 
-    })
-}
+//     })
+// }
 
-const sendCancellationEmail = (email,name)=>{
-    sgMail.send({
-        to: email,
-        from: 'umangjajodia4@gmail.com',
-        subject: 'sorry to see you go',
-        text: 'Goodbye, $(name). I hope to see you back sometime soon'
-    })
-}
-    module.exports = {
-        sendWelcomeEmail,
-        sendCancellationEmail
-    }
+// const sendCancellationEmail = (email,name)=>{
+//     sgMail.send({
+//         to: email,
+//         from: 'umangjajodia4@gmail.com',
+//         subject: 'sorry to see you go',
+//         text: 'Goodbye, $(name). I hope to see you back sometime soon'
+//     })
+// }
+//     module.exports = {
+//         sendWelcomeEmail,
+//         sendCancellationEmail
+//     }
